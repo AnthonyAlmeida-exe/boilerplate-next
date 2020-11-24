2a510d803e6f68fb5337dfe712451ef1530ccb36
Anthony Almeida <anlsilva.compasso@rd.com.br>
2020-11-24 20:19:58 -0300

    config storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,15 @@
-const Main = () => (
-  <main>
-    <h1>react avançado</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Átomo img"></S.Logo>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg"></S.Illustration>
+  </S.Wrapper>
 )
 
 export default Main
